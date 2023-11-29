352f499792713abd167357b422d8c5440363b736
Huaxu <yhxchem@outlook.com>
2023-11-29 09:57:45 -0800

    modified:   src/metabengine/annotation.py

--- src/metabengine/annotation.py
+++ src/metabengine/annotation.py
@@ -90,6 +90,54 @@ def annotate_features(feature_list, params):
                 f.formula = matched['formula'] if 'formula' in matched else None
 
 
+def annotate_features_all_mode_search(feature_list, params):
+    """
+    A function to annotate features based on their MS/MS spectra and a MS/MS database.
+    Four modes are supported: identity search, open search, neutral loss search, and hybrid search.
+    See https://www.nature.com/articles/s41592-023-02012-9 Figure 1 for more details.
+
+    Parameters
+    ----------
+    feature_list : list
+        A list of features.
+    params : Params object
+        The parameters for the workflow.
+    """
+
+    # load the MS/MS database
+    entropy_search = load_msms_db(params.msms_library)
+
+    for f in feature_list:
+
+        f.annotation = None
+        f.similarity = None
+        f.matched_peak_number = None
+        f.smiles = None
+        f.inchikey = None
+        f.matched_precursor_mz = None
+        f.matched_peaks = None
+        f.formula = None
+        
+        if f.best_ms2 is not None:
+            peaks = entropy_search.clean_spectrum_for_search(f.mz, f.best_ms2.peaks)
+            entropy_similarity, matched_peaks_number = entropy_search.search(precursor_mz=f.mz, peaks=peaks, ms1_tolerance_in_da=params.mz_tol_ms1,
+                                                                             ms2_tolerance_in_da=params.mz_tol_ms2, output_matched_peak_number=True)
+            
+            idx = np.argmax(entropy_similarity)
+            if entropy_similarity[idx] > params.ms2_sim_tol:
+                matched = entropy_search[np.argmax(entropy_similarity)]
+                matched = {k.lower():v for k,v in matched.items()}
+                f.annotation = matched['name']
+                f.similarity = entropy_similarity[idx]
+                f.matched_peak_number = matched_peaks_number[idx]
+                f.smiles = matched['smiles'] if 'smiles' in matched else None
+                f.inchikey = matched['inchikey'] if 'inchikey' in matched else None
+                f.matched_precursor_mz = matched['precursor_mz']
+                f.matched_peaks = matched['peaks']
+                f.formula = matched['formula'] if 'formula' in matched else None
+
+
+
 def annotate_rois(d):
     """
     A function to annotate rois based on their MS/MS spectra and a MS/MS database.
